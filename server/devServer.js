779c9e5fedbf448fc2b9d7a7457352b264d0e0c3
Jonathan Montoya <lonlazarus@gmail.com>
2016-12-29 18:09:24 -0800

    update dev server/bathroom

--- server/devServer.js
+++ server/devServer.js
@@ -1,6 +1,6 @@
 const WebpackDevServer = require('webpack-dev-server');
 const Webpack = require('webpack');
-const webpackConfig = require('../webpack.config.js');
+const webpackConfig = require('../webpack.dev.config.js');
 
 webpackConfig.entry.app.unshift('webpack-dev-server/client?http://localhost:8080');
 const compiler = Webpack(webpackConfig);
---
+++ webpack.dev.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const { assetPath } = require('./config');
+const BabiliPlugin = require('babili-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: ['./src/index.js'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'assets'),
+    publicPath: assetPath,
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2016'],
+        },
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+          'file?hash=sha512&digest=hex&name=[hash].[ext]',
+          'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false',
+        ],
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+};
