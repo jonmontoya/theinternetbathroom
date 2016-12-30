68da3008466a9d95eb20b9f07b21dbbef2007b25
Jonathan Montoya <lonlazarus@gmail.com>
2016-12-29 17:49:22 -0800

    minify assets

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,15 @@
 const path = require('path');
 const { assetPath } = require('./config');
+const BabiliPlugin = require('babili-webpack-plugin');
 
 module.exports = {
   entry: {
-    app: ["./src/index.js"]
+    app: ['./src/index.js'],
   },
   output: {
-    path: path.resolve(__dirname, "assets"),
+    path: path.resolve(__dirname, 'assets'),
     publicPath: assetPath,
-    filename: "bundle.js"
+    filename: 'bundle.js',
   },
   module: {
     loaders: [
@@ -17,20 +18,23 @@ module.exports = {
         exclude: /(node_modules|bower_components)/,
         loader: 'babel-loader',
         query: {
-          presets: ['es2016']
-        }
+          presets: ['es2016'],
+        },
       },
       {
         test: /\.(jpe?g|png|gif|svg)$/i,
         loaders: [
           'file?hash=sha512&digest=hex&name=[hash].[ext]',
-          'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
-        ]
+          'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false',
+        ],
       },
       {
         test: /\.scss$/,
-        loaders: ["style-loader", "css-loader", "sass-loader"]
-      }
-    ]
-  }
+        loaders: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new BabiliPlugin(),
+  ],
 };
