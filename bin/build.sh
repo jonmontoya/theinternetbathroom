319e478488d6d5be17d327af7f7eb296eba530fd
Jonathan Montoya <lonlazarus@gmail.com>
2017-01-09 22:03:55 -0800

    asset bucket by env variable

---
+++ bin/build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+rm -rf ./assets/* && ./node_modules/.bin/webpack --progress --colors
---
+++ bin/deploy.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./node_modules/.bin/s3-deploy './assets/**' --cwd './assets/' --bucket $THE_INTERNET_BATHROOM_APP_BUCKET
