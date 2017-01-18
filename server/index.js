58ffd2f0a37c66bc180054aab98b6f37017ee9d1
Jonathan Montoya <lonlazarus@gmail.com>
2017-01-18 12:43:01 -0800

    add ga script

--- server/index.js
+++ server/index.js
@@ -32,6 +32,15 @@ server.register(require('./graffitiPlugin'), () => {
           <meta property="og:url" content="http://theinternetbathroom.com"/>
           <meta property="og:image" content="${assetPath}theinternetbathroom.png"/>
           <title>The Internet Bathroom</title>
+          <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-56715351-3', 'auto');
+            ga('send', 'pageview');
+          </script>
         </head>
         <body>
           <div id="app">
